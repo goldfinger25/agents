1be565cc87e7541c99b9a3af4f47d1bfa5b50eef
alastair <alastarturton@gmail.com>
2025-08-21 20:33:55 +0100

    providers

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "rg_tfstate_01"
+      storage_account_name = "atbuildtfstate01"
+      container_name       = "tfstate"
+      key                  = "VZTPuiSZv/6dO5iyiVOAn896EQU5rXJdb5Rk3KRmD1G4KLENj9MYF5uo2aK+QOTJgHR7IG00ExAp+AStHUcUEw=="
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
