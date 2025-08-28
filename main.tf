c52ef40126ab4232465960a9404744ebb5b4db79
alastair <alastarturton@gmail.com>
2025-08-28 11:53:49 +0100

    updated files

--- main.tf
+++ main.tf
@@ -3,11 +3,9 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_subscription" "sub" {
-  alias             = "Baseline Sub"
-  subscription_name = "Baseline Sub"
-  subscription_id   = "e83a6760-22e0-44ea-a806-d6475fd8ee45"
-}
+
+
+
 resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
   location = var.location
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+
+output "current_subscription_display_name" {
+  value = data.azurerm_subscription.current.subscription_id 
+}
