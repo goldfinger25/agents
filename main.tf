0a73a7a12a52b88fb45dcb57bcd9b122a160a6db
alastair <alastarturton@gmail.com>
2025-09-03 21:31:13 +0100

    updated RG and networking

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg-agents" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg-network" {
   name     = var.resource_group_name
   location = var.location
 }
@@ -7,13 +12,13 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "devops-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-network.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "aci-subnet" {
   name                 = "aci-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg-network.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
   # Required for VNet integration
@@ -29,8 +34,8 @@ resource "azurerm_subnet" "subnet" {
 # Public IP for NAT Gateway
 resource "azurerm_public_ip" "nat_gateway_ip" {
   name                = "devops-nat-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-network.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -38,23 +43,29 @@ resource "azurerm_public_ip" "nat_gateway_ip" {
 # NAT Gateway
 resource "azurerm_nat_gateway" "nat_gateway" {
   name                    = "devops-nat-gateway"
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  #public_ip_address_ids   = [azurerm_public_ip.nat_gateway_ip.id]
-  #sku                     = "Standard"
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.rg-network.name
+  sku_name                 =  "Standard"
   idle_timeout_in_minutes = 4
+
 }
+
+  resource "azurerm_nat_gateway_public_ip_association" "example" {
+  nat_gateway_id       = nat_gateway.nat_gateway_id
+  public_ip_address_id = nat_gateway_ip.public_ip_address_id
+}
+
 
 # Associate NAT Gateway with the subnet
 resource "azurerm_subnet_nat_gateway_association" "nat_gateway_association" {
-  subnet_id       = azurerm_subnet.subnet.id
+  subnet_id       = azurerm_subnet.aci-subnet.id
   nat_gateway_id  = azurerm_nat_gateway.nat_gateway.id
 }
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg-agents.name
+  location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
 }
@@ -62,8 +73,8 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_container_group" "devops_agent" {
   count               = var.agent_count
   name                = "devops-agent-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-agents
   os_type             = "Linux"
 
   # VNet integration configuration
